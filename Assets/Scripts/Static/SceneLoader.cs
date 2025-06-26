9a2db396bda5b25622caa6204afa2e00fa716783
Julian <julian.tinapp@gmail.com>
2025-06-26 14:27:31 +0200

    added documentation for the methods

--- Assets/Scripts/Static/SceneLoader.cs
+++ Assets/Scripts/Static/SceneLoader.cs
@@ -3,11 +3,19 @@ using UnityEngine.SceneManagement;
 
 public static class SceneLoader
 {
+    /// <summary>
+    /// Load scene locally for players
+    /// </summary>
+    /// <param name="sceneName">SceneName enum contains the possible scene names</param>
     public static void LoadScene(SceneName sceneName)
     {
         SceneManager.LoadScene(sceneName.ToString());
     }
 
+    /// <summary>
+    /// Load scene for all players that are connected to the host network
+    /// </summary>
+    /// <param name="sceneName">ceneName enum contains the possible scene names</param>
     public static void LoadNetworkScene(SceneName sceneName)
     {
         NetworkManager.Singleton.SceneManager.LoadScene(sceneName.ToString(), LoadSceneMode.Single);
