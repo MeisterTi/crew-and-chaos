7ecd2cc0a803b635e2d4d2c9e54a0fc4507ec4c1
Julian <julian.tinapp@gmail.com>
2025-06-26 14:25:27 +0200

    added methods for local scene loading and network scene loading

--- Assets/Scripts/Static/SceneLoader.cs
+++ Assets/Scripts/Static/SceneLoader.cs
@@ -1,6 +1,15 @@
-using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
 public static class SceneLoader
 {
+    public static void LoadScene(SceneName sceneName)
+    {
+        SceneManager.LoadScene(sceneName.ToString());
+    }
 
+    public static void LoadNetworkScene(SceneName sceneName)
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene(sceneName.ToString(), LoadSceneMode.Single);
+    }
 }
