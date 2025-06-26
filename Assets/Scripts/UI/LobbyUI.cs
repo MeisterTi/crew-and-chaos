cbdcbdd9aa6131529707ca3fe77dac3b250ece26
Julian <julian.tinapp@gmail.com>
2025-06-26 16:15:49 +0200

    lobby script to show host ip and joind players

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -1,16 +1,30 @@
+using TMPro;
+using Unity.Netcode;
 using UnityEngine;
 
-public class LobbyUI : MonoBehaviour
+public class LobbyUI : BaseUI
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI hostIPAddressText;
+    [SerializeField] private Transform playerUIContainer;
+    [SerializeField] private Transform singlePlayerUIPrefab;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            Transform singlePlayerUITransform = Instantiate(singlePlayerUIPrefab, playerUIContainer);
+            if (singlePlayerUITransform.TryGetComponent(out SinglePlayerUI singlePlayerUI))
+            {
+                singlePlayerUI.SetPlayerName("Test");
+            }
+        }
+        if (NetworkManager.Singleton.IsServer)
+        {
+            hostIPAddressText.text = Networking.GetLocalIP();
+        }
+        else
+        {
+            hostIPAddressText.text = SceneName.Lobby.ToString();
+        }
     }
 }
