42ebff7c190cc4c96687c2b8b46fe49cc61dbbaf
Julian <julian.tinapp@gmail.com>
2025-06-26 18:37:24 +0200

    updated lobby ui, to show all players connected and there specific roles (every role is only one time assignable)

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
@@ -10,14 +11,6 @@ public class LobbyUI : BaseUI
 
     private void Awake()
     {
-        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
-        {
-            Transform singlePlayerUITransform = Instantiate(singlePlayerUIPrefab, playerUIContainer);
-            if (singlePlayerUITransform.TryGetComponent(out SinglePlayerUI singlePlayerUI))
-            {
-                singlePlayerUI.SetPlayerName("Test");
-            }
-        }
         if (NetworkManager.Singleton.IsServer)
         {
             hostIPAddressText.text = Networking.GetLocalIP();
@@ -25,6 +18,47 @@ public class LobbyUI : BaseUI
         else
         {
             hostIPAddressText.text = SceneName.Lobby.ToString();
+        }
+    }
+
+    private void Start()
+    {
+        PlayerManager.Instance.OnPlayerListChanged += PlayerManager_OnPlayerListChanged;
+        UpdatePlayerListUI();
+    }
+
+    private void PlayerManager_OnPlayerListChanged()
+    {
+        UpdatePlayerListUI();
+    }
+
+    private void UpdatePlayerListUI()
+    {
+        foreach (Transform child in playerUIContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        HashSet<SailingRole> takenRoles = new HashSet<SailingRole>();
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            PlayerData playerData = PlayerManager.Instance.GetPlayerDataByClientId(clientId);
+            if (playerData.SailingRole != SailingRole.NONE)
+            {
+                takenRoles.Add(playerData.SailingRole);
+            }
+        }
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            PlayerData playerData = PlayerManager.Instance.GetPlayerDataByClientId(clientId);
+            Transform singlePlayerUITransform = Instantiate(singlePlayerUIPrefab, playerUIContainer);
+            if (singlePlayerUITransform.TryGetComponent(out SinglePlayerUI singlePlayerUI))
+            {
+                singlePlayerUI.SetPlayerName(playerData.PlayerName.Value);
+                if (clientId == NetworkManager.Singleton.LocalClientId)
+                {
+                    singlePlayerUI.SetAvailableRoles(takenRoles);
+                }
+            }
         }
     }
 }
